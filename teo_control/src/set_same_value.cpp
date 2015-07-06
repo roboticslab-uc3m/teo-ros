85bbb1f5a996641f07b4c25294dda9e9d37e8bda
Domingo Esteban <mingo.esteban@gmail.com>
2015-07-06 19:57:35 +0200

    legs inertias have been changed

--- teo_control/src/set_same_value.cpp
+++ teo_control/src/set_same_value.cpp
@@ -22,43 +22,47 @@ Leganes, Madrid, Spain, 2015
 #include "teo_msgs/SupportFoot.h"
 
 
+#define TOTAL_TIME 2
+
 using namespace std;
 
 
-void usage(void)
-{
-    ROS_INFO("Usage:\n");
-    ROS_INFO(" -f<name>\n");
-    ROS_INFO(" -d<name>\n");
-    exit (8);
-}
+//void usage(void)
+//{
+//    ROS_INFO("Usage:\n");
+//    ROS_INFO(" -f<name>\n");
+//    ROS_INFO(" -d<name>\n");
+//    exit (8);
+//}
 
 
 //MAIN FUNCTION
 int main(int argc, char **argv)
 {
 
-    for (int i = 1; i < argc; i++) {
-        if (i + 1 != argc) // Check that we haven't finished parsing already
-            if (argv[i] == "-f") {
-                // We know the next argument *should* be the filename:
-                myFile = argv[i + 1];
-            } else if (argv[i] == "-p") {
-                myPath = argv[i + 1];
-            } else if (argv[i] == "-o") {
-                myOutPath = argv[i + 1];
-            } else {
-                std::cout << "Not enough or invalid arguments, please try again.\n";
-                Sleep(2000);
-                exit(0);
-        }
-        std::cout << argv[i] << " ";
-    }
+//    Incluir tiempo de publicacion, frecuencia y/u otros
+//    for (int i = 1; i < argc; i++) {
+//        if (i + 1 != argc) // Check that we haven't finished parsing already
+//            if (argv[i] == "-f") {
+//                // We know the next argument *should* be the filename:
+//                myFile = argv[i + 1];
+//            } else if (argv[i] == "-p") {
+//                myPath = argv[i + 1];
+//            } else if (argv[i] == "-o") {
+//                myOutPath = argv[i + 1];
+//            } else {
+//                std::cout << "Not enough or invalid arguments, please try again.\n";
+//                Sleep(2000);
+//                exit(0);
+//        }
+//        std::cout << argv[i] << " ";
+//    }
 
   // Initialize Ros Node
   ros::init(argc, argv, "TEOSetPointController");
   ros::NodeHandle n;	
-  ros::Time::init();
+
+//  ros::Time::init();
 
   
 
@@ -91,197 +95,54 @@ int main(int argc, char **argv)
   ros::Publisher joint24_command_to_ROS = n.advertise<std_msgs::Float64>("/teo/l_wrist_yaw_position_controller/command", 1);
   ros::Publisher joint25_command_to_ROS = n.advertise<std_msgs::Float64>("/teo/l_wrist_pitch_position_controller/command", 1);
 
-  std_msgs::Float64 joint_command;
 
+  std_msgs::Float64 joint_command;
+  joint_command.data = 0;
+
+
+  ros::Time begin = ros::Time::now();
+  ros::Duration duration;
 
   // ROS
   while(n.ok()) {
-    if (client_sockfd == -1) {
-      //Ask O.S. for conections and accept one
-      sin_size = sizeof(struct sockaddr_in);
-      ROS_INFO("Waiting for TEOTraGen trajectories...");
-      if ((client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_addr,
-                          &sin_size))==-1) {
-        ROS_ERROR("Error in accept()\n");
-      	close(server_sockfd);
-        exit(-1);
-      }
-      ROS_INFO("Receiving trajectories from TEOTraGen ...");
-      usleep(2000); // Wait 2 ms
-    }
-    else {
-      if ((numbytes_received = recv(client_sockfd, buffer_received, sizeof(struct Humanoid_joints_structure), 0)) == -1){  
-        printf("Error in recv() \n");
-	      close(server_sockfd); close(client_sockfd);
-        client_sockfd=-1;
-        exit(-1);
-      }
-      if (numbytes_received == 0) {
-        ROS_WARN("Comunication with TEOTraGen lost... \n");
-        close(client_sockfd);
-        client_sockfd=-1;
-
-        // Temporal
-
-        usleep(2000000); // Wait 2 s
-        joint_command.data = 0;
-        for (int ii=0; ii<14; ii++) {
-            joint0_command_to_ROS.publish(joint_command);
-
-            joint1_command_to_ROS.publish(joint_command);
-
-            joint2_command_to_ROS.publish(joint_command);
-
-            joint3_command_to_ROS.publish(joint_command);
-
-            joint4_command_to_ROS.publish(joint_command);
-
-            joint5_command_to_ROS.publish(joint_command);
-
-            joint6_command_to_ROS.publish(joint_command);
-
-            joint7_command_to_ROS.publish(joint_command);
-
-            joint8_command_to_ROS.publish(joint_command);
-
-            joint9_command_to_ROS.publish(joint_command);
-
-            joint10_command_to_ROS.publish(joint_command);
-
-            joint11_command_to_ROS.publish(joint_command);
-
-            joint12_command_to_ROS.publish(joint_command);
-        }
-
-
-      }
-      else {
-        num_config_received += 1;
-        TEO_joints = *(struct Humanoid_joints_structure *) &buffer_received;
-        //printf("Recibido dato %lu con %d Bytes\n",num_config_received, numbytes_received);
-
-        joints_to_send.header.stamp = ros::Time::now();
-        //ROS_WARN("SF es %d TEO_joints.support_foot \n", TEO_joints.support_foot);
-
-        joints_to_send.name.resize ( 0 );
-        joints_to_send.position.resize ( 0 );
-        joints_to_send.velocity.resize ( 0 );
-        joints_to_send.effort.resize ( 0 );
-
-        for (int ii=0; ii<26; ii++) {
-          joint_command.data = TEO_joints.joints[ii];
-
-
+      for (int ii=0; ii<14; ii++) {
           joint0_command_to_ROS.publish(joint_command);
 
+          joint1_command_to_ROS.publish(joint_command);
 
-          switch (ii) {
-            case 0:
-              joints_to_send.name.push_back("r_hip_yaw");
-              joint0_command_to_ROS.publish(joint_command);
-              break;
-            case 1:
-              joints_to_send.name.push_back("r_hip_roll");
-              joint1_command_to_ROS.publish(joint_command);
-              break;
-            case 2:
-              joints_to_send.name.push_back("r_hip_pitch");
-              joint2_command_to_ROS.publish(joint_command);
-              break;
-            case 3:
-              joints_to_send.name.push_back("r_knee_pitch");
-              joint3_command_to_ROS.publish(joint_command);
-              break;
-            case 4:
-              joints_to_send.name.push_back("r_ankle_pitch");
-              joint4_command_to_ROS.publish(joint_command);
-              break;
-            case 5:
-              joints_to_send.name.push_back("r_ankle_roll");
-              joint5_command_to_ROS.publish(joint_command);
-              break;
-            case 6:
-              joints_to_send.name.push_back("l_hip_yaw");
-              joint6_command_to_ROS.publish(joint_command);
-              break;
-            case 7:
-              joints_to_send.name.push_back("l_hip_roll");
-              joint7_command_to_ROS.publish(joint_command);
-              break;
-            case 8:
-              joints_to_send.name.push_back("l_hip_pitch");
-              joint8_command_to_ROS.publish(joint_command);
-              break;
-            case 9:
-              joints_to_send.name.push_back("l_knee_pitch");
-              joint9_command_to_ROS.publish(joint_command);
-              break;
-            case 10:
-              joints_to_send.name.push_back("l_ankle_pitch");
-              joint10_command_to_ROS.publish(joint_command);
-              break;
-            case 11:
-              joints_to_send.name.push_back("l_ankle_roll");
-              joint11_command_to_ROS.publish(joint_command);
-              break;
-            case 12:
-              joints_to_send.name.push_back("waist_yaw");
-              joint12_command_to_ROS.publish(joint_command);
-              break;
-            case 13:
-              joints_to_send.name.push_back("waist_pitch");
-              break;
-            case 14:
-              joints_to_send.name.push_back("r_shoulder_pitch");
-              break;
-            case 15:
-              joints_to_send.name.push_back("r_shoulder_roll");
-              break;
-            case 16:
-              joints_to_send.name.push_back("r_shoulder_yaw");
-              break;
-            case 17:
-              joints_to_send.name.push_back("r_elbow_pitch");
-              break;
-            case 18:
-              joints_to_send.name.push_back("r_wrist_yaw");
-              break;
-            case 19:
-              joints_to_send.name.push_back("r_wrist_pitch");
-              break;
-            case 20:
-              joints_to_send.name.push_back("l_shoulder_pitch");
-              break;
-            case 21:
-              joints_to_send.name.push_back("l_shoulder_roll");
-              break;
-            case 22:
-              joints_to_send.name.push_back("l_shoulder_yaw");
-              break;
-            case 23:
-              joints_to_send.name.push_back("l_elbow_pitch");
-              break;
-            case 24:
-              joints_to_send.name.push_back("l_wrist_yaw");
-              break;
-            case 25:
-              joints_to_send.name.push_back("l_wrist_pitch");
-              break;
-          }
+          joint2_command_to_ROS.publish(joint_command);
 
+          joint3_command_to_ROS.publish(joint_command);
 
-        }
+          joint4_command_to_ROS.publish(joint_command);
 
+          joint5_command_to_ROS.publish(joint_command);
 
+          joint6_command_to_ROS.publish(joint_command);
+
+          joint7_command_to_ROS.publish(joint_command);
+
+          joint8_command_to_ROS.publish(joint_command);
+
+          joint9_command_to_ROS.publish(joint_command);
+
+          joint10_command_to_ROS.publish(joint_command);
+
+          joint11_command_to_ROS.publish(joint_command);
+
+          joint12_command_to_ROS.publish(joint_command);
       }
-    }
+
+
+      duration = ros::Time::now() - begin;
+//      if (duration.toSec() >= TOTAL_TIME)
+//        break;
+
+
 
     ros::spinOnce();
-
   }
 
 
   return 0;
 }
-
-
